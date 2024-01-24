90560f20ff14aedd946e59eae9208a96e9e1e007
AYISSI ESSOMBA Herve <herveae@gmail.com>
2024-01-24 04:41:54 +0100

    Ajout de la fonctionnalite de connexion de l'administrateur

--- connexionAdmin.php
+++ connexionAdmin.php
@@ -1,3 +1,36 @@
+<?php
+    session_start();
+    include_once("DBconnexion.php");
+    $pdo= new connect();
+
+    if(isset($_POST['envoi'])){
+        if(!empty($_POST['login']) AND !empty($_POST['password'])){
+            $login=htmlspecialchars($_POST['login']);
+            $password=htmlspecialchars($_POST['password']);
+
+            $query="SELECT * FROM admins WHERE login=? AND password=?";
+            $recupAdmins=$pdo->prepare($query);
+            $recupAdmins->execute(array($login,$password));
+
+            if($recupAdmins->rowCount()>0){
+                $_SESSION['login']=$login;
+                $_SESSION['password']=$password;
+                $_SESSION['id'] =$recupAdmins->fetch()['id_admins'];
+                header('location: dashboard.php');
+            }else{
+                echo "Votre mot de passe, votre email est incorrect";
+            }
+        }else{
+            echo "Veuillez completer tout les champs...";
+        }
+    }
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -12,46 +45,25 @@
     <link rel="stylesheet" href="assets/styles/style.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-          <a class="navbar-brand" href="#">Newse</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Aide</a>
-              </li>
-              
-            <!-- <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> -->
-          </div>
-        </div>
-      </nav>
+    <?php include_once("navbarHome.php");?>
 
       <main class="" id="main">
         <div class="container connexion">
             <div class="row">
                 <div class="col-lg-5 mx-auto">
-                    <form>
+                    <form method="POST">
                         <h1 class="h3 mb-3 text-center">Heureux de vous revoir</h1>
                     
                         <div class="form-floating mb-3">
-                          <input type="text" class="form-control" id="floatingInput" placeholder="Login" required>
+                          <input name="login" type="text" class="form-control" id="floatingInput" placeholder="Login" required>
                           <label for="floatingInput">Login</label>
                         </div>
                         <div class="form-floating mb-3">
-                          <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+                          <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
                           <label for="floatingPassword">Password</label>
                         </div>
 
-                        <a href="dashboard.php" class="w-100 btn btn-lg btn-dark mt-3" type="submit">Sign in</a>
+                        <button name="envoi" class="w-100 btn btn-lg btn-dark mt-3" type="submit">Sign in</button>
                       </form>
                 </div>
             </div>
--- homeUsers.php
+++ homeUsers.php
@@ -18,6 +18,7 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+          
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
               <li class="nav-item">
@@ -37,8 +38,9 @@
               <button class="btn btn-outline-success" type="submit">Search</button>
             </form> -->
           </div>
+          <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#deconnexion">Deconnexion</a>
           
-            <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#deconnexion">Deconnexion</a>
+            
       
         </div>
       </nav>
